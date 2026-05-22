0a85234d7e9bcb4c012c858824c8de08730b2529
pycogram <kaluifesinachi@yahoo.com>
2026-05-22 04:01:37 +0100

    feat: update docs page

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Docs from "./docs-content";
 export const metadata: Metadata = {
   alternates: {
-    canonical: "https://www.agentropic.org/docs",
+    canonical: "https://www.zeroicai.org/docs",
   },
 };
 
